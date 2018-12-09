25189c74d4fd6fd4263825e32f3208f713fa82c9
Avinash Inuganti <inuganti@ebi.ac.uk>
2018-12-09 17:23:12 +0000

    tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.example.demo.model.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,6 +23,9 @@ public class DemoApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@Test
 	public void getProductsTest() {
@@ -59,6 +63,18 @@ public class DemoApplicationTests {
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	public void updatePriceTest() throws Exception{
+		String insertProductJson = "{ \"name\": \"pen\", \"price\": 2.3 }";
+		MvcResult result = this.mockMvc.perform(post("/products/").contentType(MediaType.APPLICATION_JSON)
+				.content(insertProductJson)).andDo(print()).andExpect(status().isOk()).andReturn();
+		System.out.println(result.getResponse().getContentAsString());
+		Product savedProduct = objectMapper.readValue(result.getResponse().getContentAsString(), Product.class);
+		savedProduct.setPrice(2.5);
+		this.mockMvc.perform(put("/products/"+savedProduct.getId()).contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(savedProduct))).andDo(print()).andExpect(status().isOk()).andReturn();
 	}
 
 }
