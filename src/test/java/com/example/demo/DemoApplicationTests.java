268bdfe8610c238d9860a37c3f4f88886bc5597d
Avinash Inuganti <inuganti@ebi.ac.uk>
2018-12-09 17:04:49 +0000

    tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,35 +1,64 @@
 package com.example.demo;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DemoApplicationTests {
 
-	MockMvc mockMvc;
-
 	@Autowired
-	private WebApplicationContext context;
+	private MockMvc mockMvc;
 
-	@Before
-	public void setUp() {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	@Test
+	public void getProductsTest() {
+		try{
+			this.mockMvc.perform(get("/products/")).andDo(print()).andExpect(status().isOk());
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	@Test
-	public void contextLoads() {
+	public void saveProductsValidationErrorTest() {
+		try{
+			this.mockMvc.perform(post("/products/").contentType(MediaType.APPLICATION_JSON)
+					.content("{ \"name\": \"pen\", \"price\": -1 }")).andDo(print()).andExpect(status().is5xxServerError());
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
 
+	@Test
+	public void saveProductsValidTest() {
+		try{
+			this.mockMvc.perform(post("/products/").contentType(MediaType.APPLICATION_JSON)
+					.content("{ \"name\": \"pen\", \"price\": 2.3 }")).andDo(print()).andExpect(status().isOk());
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void deleteProductTest(){
+		try{
+			this.mockMvc.perform(delete("/products/1")).andDo(print()).andExpect(status().isOk());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
