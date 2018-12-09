293322e59c6ebded63c4471a5f9625bb5c760324
avinash-dev <avinash.cloud9@gmail.com>
2018-12-09 23:43:17 +0530

    updated tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.model.Order;
 import com.example.demo.model.Product;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
@@ -12,8 +13,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -74,7 +78,45 @@ public class DemoApplicationTests {
 		Product savedProduct = objectMapper.readValue(result.getResponse().getContentAsString(), Product.class);
 		savedProduct.setPrice(2.5);
 		this.mockMvc.perform(put("/products/"+savedProduct.getId()).contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(savedProduct))).andDo(print()).andExpect(status().isOk()).andReturn();
+				.content(objectMapper.writeValueAsString(savedProduct))).andDo(print()).andExpect(status().isOk()).andExpect(content().json(objectMapper.writeValueAsString(savedProduct)));
 	}
+	
+	@Test
+	public void insertOrderTest() throws Exception{
+		String insertProductJson = "{ \"name\": \"pen\", \"price\": 2.3 }";
+		MvcResult result = this.mockMvc.perform(post("/products/").contentType(MediaType.APPLICATION_JSON)
+				.content(insertProductJson)).andDo(print()).andExpect(status().isOk()).andReturn();
+		String orderJson = "{ \"email\":\"abc@def.com\", \"paymentSuccessFul\":true, \"shoppingCartItemsList\":[ { \"quantity\":5, \"product\":{ \"id\":1 } } ] }";
+		MvcResult orderResult = this.mockMvc.perform(post("/orders/").contentType(MediaType.APPLICATION_JSON)
+				.content(orderJson)).andDo(print()).andExpect(status().isOk()).andReturn();
+		System.out.println(orderResult.getResponse().getContentAsString());
+		Order savedOrder = objectMapper.readValue(orderResult.getResponse().getContentAsString(), Order.class);
+		assertTrue(savedOrder.getOrderTotalPrice()==(2.3*5));
+	}
+	
+	@Test
+	public void updateProductOrderPriceTest() throws Exception {
+		//insert product
+		String insertProductJson = "{ \"name\": \"pen\", \"price\": 2.3 }";
+		MvcResult result = this.mockMvc.perform(post("/products/").contentType(MediaType.APPLICATION_JSON)
+				.content(insertProductJson)).andDo(print()).andExpect(status().isOk()).andReturn();
+		//insert order containing the previous product
+		String orderJson = "{ \"email\":\"abc@def.com\", \"paymentSuccessFul\":true, \"shoppingCartItemsList\":[ { \"quantity\":5, \"product\":{ \"id\":1 } } ] }";
+		MvcResult orderResult = this.mockMvc.perform(post("/orders/").contentType(MediaType.APPLICATION_JSON)
+				.content(orderJson)).andDo(print()).andExpect(status().isOk()).andReturn();
+		System.out.println(orderResult.getResponse().getContentAsString());
+		Order savedOrder = objectMapper.readValue(orderResult.getResponse().getContentAsString(), Order.class);
+		//update product price
+		insertProductJson = "{ \"name\": \"pen\", \"price\": 2.1 }";
+		this.mockMvc.perform(put("/products/1").contentType(MediaType.APPLICATION_JSON)
+				.content(insertProductJson)).andDo(print()).andExpect(status().isOk()).andReturn();
+		//check order's totalprice - should remain same
+		orderResult = this.mockMvc.perform(get("/orders/"+savedOrder.getId()).contentType(MediaType.APPLICATION_JSON)
+				.content(orderJson)).andDo(print()).andExpect(status().isOk()).andReturn();
+		System.out.println(orderResult.getResponse().getContentAsString());
+		savedOrder = objectMapper.readValue(orderResult.getResponse().getContentAsString(), Order.class);
+		assertTrue(savedOrder.getOrderTotalPrice()==(2.3*5));
+	}
+	
 
 }
